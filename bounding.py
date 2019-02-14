3afa6381181cd74dbdd2f80d04c5a33874470906
Abhay Alekal <abhayalekal74@gmail.com>
2019-02-14 11:31:04 +0530

    X Based merging of lines

--- bounding.py
+++ bounding.py
@@ -1,4 +1,6 @@
-VERTICAL_THRES = 10
+import math
+
+VERTICAL_THRES = 2 
 HORIZ_THRES = 16
 
 class Bound:
@@ -12,6 +14,48 @@ class Bound:
 
 	def print(self):
 		print (self.word, self.left, self.top, self.right, self.bottom, self.bound_merged)
+
+
+class XPosMap:
+	def __init__(self):
+		self.max_x = 0
+		self.pos_map = dict()
+	
+	def print(self):
+		print ("\n\nmax_x", self.max_x)
+		for k, v in self.pos_map.items():
+			print (k) 
+			[b.print() for b in v]
+
+
+def update_x_pos_batches(x_pos_map, cur_merged_batch):
+	if len(cur_merged_batch) > 0:
+		max_right = math.ceil(cur_merged_batch[-1].right / 100.0)
+		print ("max_right", cur_merged_batch[-1].right, max_right, x_pos_map.pos_map)
+
+		for i in range(x_pos_map.max_x + 1, max_right + 1):
+			x_pos_map.pos_map[i] = []
+
+		if max_right > x_pos_map.max_x: 
+			x_pos_map.max_x = max_right
+
+		for batch in cur_merged_batch:
+			left = math.ceil(batch.left / 100.0)
+			right = math.ceil(batch.right / 100.0)
+			print (batch.right)
+			x_pos_map.pos_map[left].append(batch)
+			x_pos_map.pos_map[right].append(batch)
+
+
+def get_bounds_in_range(bound_of_next_batch, x_pos_map):
+	bounds_in_range = list()
+	for i in range(math.ceil(bound_of_next_batch.left / 100.0), math.ceil(bound_of_next_batch.right / 100.0) + 1):
+		try:
+			bounds_in_range += x_pos_map.pos_map[i]
+		except KeyError: # In case the line above contains lesser words
+			break
+	return bounds_in_range
+
 
 def merge_bounds(bounds):
 
@@ -37,28 +81,59 @@ def merge_bounds(bounds):
 			batches.append(bounds[bounds_counter])
 			bounds_counter += 1
 		if len(batches) > 0:
+			batches.sort(key=lambda x: x.left)
 			batched_bounds.append([rect_bottoms[rect_bot_counter], bounds[bounds_counter - 1].top, batches]) # Adding this to the list of batches of top
 		rect_bot_counter += 1
+
+	print ("\n\n", len(batched_bounds), "Batched bounds\n")
+	for bound in batched_bounds:
+		[b.print() for b in bound[2]]
+		print ()
 
 	# Merge fractions if present
 	merged_batch_bounds = list()
 	i = 0 
 	while i < len(batched_bounds):
 		cur_merged_batch = list()
+		x_pos_map = XPosMap()
 		cur_merged_batch += batched_bounds[i][2]
+		update_x_pos_batches(x_pos_map, cur_merged_batch)
 		j = i + 1
 		while j < len(batched_bounds):
-			cur_batch_max_bottom = max([b.bottom for b in cur_merged_batch])
-			next_batch_min_top = min([b.top for b in batched_bounds[j][2]])
-			if (next_batch_min_top - cur_batch_max_bottom) < VERTICAL_THRES:
+			next_batched_bounds = batched_bounds[j][2]
+
+			first_bound_of_next_batch = next_batched_bounds[0]
+			last_bound_of_next_batch = next_batched_bounds[-1]
+			cur_batch_bounds_in_range_for_first_bound = get_bounds_in_range(first_bound_of_next_batch, x_pos_map)
+			cur_batch_bounds_in_range_for_last_bound = get_bounds_in_range(last_bound_of_next_batch, x_pos_map) 
+
+			print ("first_bound_of_next_batch")
+			first_bound_of_next_batch.print()
+			cur_batch_bounds_in_range_for_first_bound[0].print()
+
+			print ("last_bound_of_next_batch")
+			last_bound_of_next_batch.print()
+			cur_batch_bounds_in_range_for_first_bound[-1].print()
+
+			print (first_bound_of_next_batch.top - cur_batch_bounds_in_range_for_first_bound[0].bottom)
+
+			if abs(first_bound_of_next_batch.top - cur_batch_bounds_in_range_for_first_bound[0].bottom) < VERTICAL_THRES and abs(last_bound_of_next_batch.top < cur_batch_bounds_in_range_for_last_bound[-1].bottom) < VERTICAL_THRES:
 				cur_merged_batch += batched_bounds[j][2]
+				update_x_pos_batches(x_pos_map, cur_merged_batch)
 				j += 1
 				i += 1
 			else:
 				break
+
+		x_pos_map.print()
 		if len(cur_merged_batch) > 0:
 			merged_batch_bounds.append(cur_merged_batch)
 		i += 1
+
+	print ("\n\nMerged bounds\n")
+	for bound in merged_batch_bounds:
+		[b.print() for b in bound]
+		print ()
 
 	"""
 	Ordering words within a line
